8b26a4529f7f67c488213d2c939f49ffe215672d
Ítalo David <italo.silva@univille.br>
2026-09-20 14:00:31 -0300

    chore: estrutura o projeto em backend/ para acomodar frontend separado

---
+++ src/test/java/springpagina/web/financeiro_controle/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package springpagina.web.financeiro_controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
